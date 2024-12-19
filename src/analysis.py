5622b815d4b17af3ea0f646ac4ca17980e76743c
ramity <lewisclbrown@gmail.com>
2024-12-19 18:25:30 +0000

    resolved some errors with analysis and eval files

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,17 @@
 import matplotlib
+import pickle
 
-with open(cache_path, "wb") as file:
-            pickle.dump(performance_timings, file)
+cache_path = "performance-timings.pkl"
+performance_timings = {}
+with open(cache_path, "r") as file:
+    pickle.dump(performance_timings, file)
+
+for url, metrics in performance_timings:
+
+    time_to_interactive = metrics["domInteractive"] - metrics["navigationStart"]
+    dom_content_load_time = metrics["domContentLoadedEventEnd"] - metrics["navigationStart"]
+    render_time = metrics["domComplete"] - metrics["navigationComplete"]
+    total_page_load_time = metrics["loadEventEnd"] - metrics["navigationStart"]
+
+    print(time_to_interactive, dom_content_load_time, render_time, total_page_load_time)
+    break
--- src/evaluate.py
+++ src/evaluate.py
@@ -13,10 +13,12 @@ with sync_playwright() as p:
 
     # Load file into context if it already exists
     if os.path.exists(cache_path):
-        with open(cache_path, "wb") as file:
-            pickle.dump(performance_timings, file)
+        with open(cache_path, "rb") as file:
+            performance_timings = pickle.load(file)
 
     for divs in range(1000, 10100, 100):
+
+        print(f"https://ramity.github.io/how-many-divs/{divs}")
 
         # Skip clause - Skip if divs category has already been successful
         if f"https://ramity.github.io/how-many-divs/{divs}-35px" in performance_timings:
@@ -30,7 +32,6 @@ with sync_playwright() as p:
             url = f"https://ramity.github.io/how-many-divs/{divs}-{height}px"
             page.goto(url)
             performance_timings[url] = page.evaluate("() => performance.timing")
-            print(f"{url} Performance Timing: {performance_timings[url]}")
 
         # Update saved file
         with open(cache_path, "wb") as file:

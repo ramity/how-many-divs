24c0353508704a035a9a330e23d8760e807cfcc8
ramity <lewisclbrown@gmail.com>
2024-12-19 16:06:53 +0000

    improved eval to cache using a pickle :3

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,19 +4,36 @@ import pickle
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
+    context = browser.new_context()
+
+    # Init cache
+    cache_path = "performance-timings.pkl"
     performance_timings = {}
+    with open(cache_path, "wb") as file:
+            pickle.dump(performance_timings, file)
 
     for divs in range(1000, 10100, 100):
+
+        # Load inprogress file
+        with open(cache_path, "rb") as file:
+            performance_timings = pickle.load(file)
+
+        # Skip clause - Skip if divs category has already been successful
+        if f"https://ramity.github.io/how-many-divs/{divs}-35px" in performance_timings:
+            print(f"Skipping divs {divs} - a")
+            continue
+
+        # Logic
         for height in range(1, 36):
 
-            context = browser.new_context()
             page = context.new_page()
-
             url = f"https://ramity.github.io/how-many-divs/{divs}-{height}px"
             page.goto(url)
             performance_timings[url] = page.evaluate("() => performance.timing")
             print(f"{url} Performance Timing: {performance_timings[url]}")
 
+        # Update saved file
+        with open(cache_path, "wb") as file:
+            pickle.dump(performance_timings, file)
+
     browser.close()
-    with open("performance-timings.pkl", "wb") as file:
-        pickle.dump(performance_timings, file)
